07f83e9368b4911da3084ea9da88101cc0681249
Shiro <honguyen25121991@gmail.com>
2022-03-20 18:14:40 +0700

    update

--- src/screens/HomeScreens/index.js
+++ src/screens/HomeScreens/index.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 import {BackgroundView, Text, Header} from '../../components';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image, Dimensions} from 'react-native';
+import axios from 'axios';
+const {height: sHeight, width: sWidth} = Dimensions.get('window');
 
 export default class HomeScreen extends Component {
+  state = {
+    loading: true,
+    game: {},
+  };
   renderLeftComponent = (
     <View>
       <Text header>
@@ -11,14 +17,62 @@ export default class HomeScreen extends Component {
       <Text>Best Game for today</Text>
     </View>
   );
-
+  componentDidMount() {
+    axios({method: 'GET', url: 'http://10.0.2.2:3000/games'})
+      .then(res => this.setState({game: res.data[0], loading: false}))
+      .catch(err => console.error(err));
+  }
   render() {
+    console.log(this.state);
+    console.log(sWidth, sHeight);
+
+    const {
+      game: {title, subTitle, icon, preview, backgroundColor},
+      loading,
+    } = this.state;
+
     return (
       <BackgroundView>
         <Header
           LeftComponent={this.renderLeftComponent}
           RightComponent={<View style={styles.avatar} />}
         />
+        {!loading && (
+          <View>
+            <Image
+              source={{
+                uri: preview[0],
+              }}
+              style={{width: sWidth, height: 200, backgroundColor: '#bbb'}}
+            />
+            <View
+              style={{
+                flexDirection: 'row',
+                backgroundColor,
+                marginHorizontal: 20,
+                paddingHorizontal: 20,
+                paddingVertical: 15,
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={{
+                  uri: icon,
+                }}
+                style={{
+                  width: 70,
+                  height: 70,
+                  borderRadius: 8,
+                  backgroundColor: '#bbb',
+                }}
+              />
+              <View style={{width: '70%'}}>
+                <Text bold>{title}</Text>
+                <Text>{subTitle}</Text>
+              </View>
+            </View>
+          </View>
+        )}
       </BackgroundView>
     );
   }
--- src/screens/StreamScreens/index.js
+++ src/screens/StreamScreens/index.js
@@ -1,11 +1,18 @@
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import React, {Component} from 'react';
 
 export default class StreamSreens extends Component {
   render() {
     return (
       <View>
-        <Text>StreamSreens</Text>
+        <Text>StreamSreens1</Text>
+        <Image
+          source={{
+            uri: 'http://10.0.2.2:3000/images/Alto_0.png',
+          }}
+          style={{width: '100%', height: 200}}
+        />
+        <Text>1243</Text>
       </View>
     );
   }
